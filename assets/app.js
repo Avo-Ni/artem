cf5e16ccd225cb61a2e421616b897a74ab3651d6
Niavo Gael <gaelmanantena@gmail.com>
2022-12-14 20:04:36 +0300

    14-12-2022

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
-import {createRoot} from "reac-dom";
+import ReactDom from 'react-dom';
+
 require ('../assets/styles/app.css');
  
 console.log('Hello World Test Console');
 
 const App = () => {
-    return 
-    <h1>Bonjour test</h1>;
+    return <h1>Bonjour test</h1>;
 };
 
 const rootElement = document.querySelector('#app');
-createRoot(<App />, rootElement);
+ReactDom.render(<App />, rootElement);
